0b2904ff9b3a55fbda9c941363bfde2c2932ccb1
Das <k.dasaprakash@gmail.com>
2018-03-08 22:00:37 +0530

    Updated Logistic Regression

--- ML_Basics/Logistic_Regression.py
+++ ML_Basics/Logistic_Regression.py
@@ -15,7 +15,7 @@ class LogisticRegression:
     def fit(self, X, Y):
         self.X = X
         self.Y = Y
-        self.theta = np.array([0.2, 0.2, -24])
+        self.theta = np.array([0, 0, -25])
         train = Train(X, Y)
         self.theta = train.gradient_descent(self.theta)
         
@@ -40,21 +40,25 @@ class Train:
     #Cross Entropy
     def computeCost(self, theta):
         Yhat = self.sigmoid(self.X, theta)
-        J = (-1)*(np.dot(self.Y, np.log(Yhat)) + np.dot((1-self.Y), np.log(1-Yhat)))/self.Y.size
+        l1 = np.log(Yhat)
+        l2 = np.log(1-Yhat)
+        J = (-1)*(np.dot(self.Y, l1) + np.dot((1-self.Y), l2))/self.Y.size
         return J.flatten()
 
     #calculate weights
     def computeGradient(self, theta):
-        new_theta = np.zeros(self.X.shape[1])
+        #new_theta = np.zeros(self.X.shape[1])
         Yhat = self.sigmoid(self.X, theta)
-        for i in range(theta.size):
-            new_theta[i] = np.sum((Yhat-self.Y)*self.X[:,i])/self.Y.size
-        for i in range(theta.size):
-            theta[i] = new_theta[i]
-        return theta
+        new_theta = self.X.T.dot(Yhat-self.Y)/self.Y.size
+        return new_theta
 
     def gradient_descent(self, theta):
         return fmin_bfgs(self.computeCost, x0=theta, maxiter=400, fprime=self.computeGradient)
+
+def normalize(X):
+    for i in range(X.shape[1]-1):
+        X[:,i] = (X[:,i] - np.mean(X[:,i]))/np.std(X[:,i])
+    return X
 
 #Load and prepare data
 df = pd.read_csv('Logistic_regression.txt', sep=',', header=None)
@@ -63,6 +67,8 @@ X = df.iloc[:, 0:-1]
 Y = df.iloc[:, -1]
 X = np.array(X)
 Y = np.array(Y)
+
+X = normalize(X)
 
 model = LogisticRegression()
 model.fit(X, Y)
