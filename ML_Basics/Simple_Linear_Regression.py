00991b9ed34ac4aaf3e71a243f049626ebeee8b7
Das <k.dasaprakash@gmail.com>
2018-03-08 19:09:59 +0530

    Updated Simple LR

--- ML_Basics/Simple_Linear_Regression.py
+++ ML_Basics/Simple_Linear_Regression.py
@@ -14,6 +14,7 @@ class SimpleLinearRegression:
         num_iter = 1500
         train = Train(self.X, self.Y)
         self.theta, self.cost_history = train.gradient_descent(theta, alpha, num_iter, regularization)
+        return self.cost_history
     
     def predict(self, X):
         test = Test(X)
@@ -24,24 +25,6 @@ class SimpleLinearRegression:
     def cost(self):
         train = Train(self.X, self.Y)
         return train.cost_function(self.theta)
-    
-    def plot_cost(self):
-        plt.grid(True)
-        plt.plot(self.cost_history)
-        plt.xlabel('Iterations')
-        plt.ylabel('Cost')
-        plt.show()
-        
-    #Plot data
-    def plotData(self,X, Y, Yhat):
-        plt.grid(True)
-        plt.scatter(X, Y, marker='x', color = 'red')
-        plt.plot(X, Yhat)
-        plt.xlim(4, 24)
-        plt.ylim(ymin=0)
-        plt.xlabel('Input')
-        plt.ylabel('Output')
-        plt.show()
         
     def score(self, Y_test, Y_pred):
         sserror = np.sum((Y_test - Y_pred)**2)
@@ -58,6 +41,7 @@ class Train:
         new_theta[0] = theta[0]
         new_theta[1] = theta[1]
         cost_history = []
+        visualization = Visualize()
         l = 1e-3
         for j in range(num_iter):
             for i in range(theta.size):
@@ -72,6 +56,8 @@ class Train:
             if j%10 == 0:
                 cost = self.cost_function(theta)
                 cost_history.append(cost)
+            if j%100 == 0:
+                visualization.plotData(self.X[:,1], self.Y, np.dot(self.X, theta))
         return theta, cost_history
     
     def cost_function(self, theta):
@@ -83,6 +69,25 @@ class Test:
     
     def get_predictions(self, theta):
         return np.dot(self.X, theta)
+    
+class Visualize():
+    def plot_cost(self, cost_history):
+        plt.grid(True)
+        plt.plot(cost_history)
+        plt.xlabel('Iterations')
+        plt.ylabel('Cost')
+        plt.show()
+        
+    #Plot data
+    def plotData(self,X, Y, Yhat):
+        plt.grid(True)
+        plt.scatter(X, Y, marker='x', color = 'red')
+        plt.plot(X, Yhat)
+        plt.xlim(4, 24)
+        plt.ylim(ymin=0)
+        plt.xlabel('Input')
+        plt.ylabel('Output')
+        plt.show()
 
 df = pd.read_csv("Simple_LR.txt", sep=',', header=None)
 df.insert(0, 'B', np.ones(df.shape[0]))
@@ -93,14 +98,15 @@ Y = np.array(df.iloc[:, -1])
 
 #model fit
 model = SimpleLinearRegression()
-model.fit(X, Y, regularization='l1')
+cost_history = model.fit(X, Y, regularization='l1')
 print("Training Cost: ", model.cost())
 Y_pred = model.predict(X)
 print("Training Score: ", model.score(Y, Y_pred))
 
 #Plot
-model.plotData(X[:, 1], Y, Y_pred)
-model.plot_cost()
+visualization = Visualize()
+visualization.plotData(X[:, 1], Y, Y_pred)
+visualization.plot_cost(cost_history)
 
 #Predict
 predict1 = model.predict([[1, 3.5]])
@@ -111,12 +117,3 @@ print('For population = 70,000, we predict a profit of ' + str(predict2*10000))
 test = Test(X)
 yh1 = test.get_predictions([-2, 0.5])
 yh2 = test.get_predictions([-3.63023346, 1.16635641])
-
-plt.grid(True)
-plt.scatter(X[:, 1], Y, marker='x', color = 'red')
-plt.plot(X[:, 1], yh1, 'g', X[:, 1], yh2, 'b')
-plt.xlim(4, 24)
-plt.ylim(ymin=0)
-plt.xlabel('Input')
-plt.ylabel('Output')
-plt.show()
