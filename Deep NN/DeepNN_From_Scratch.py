e6ead1c6c918303757334eb8ea381ab5e3f723f3
Das <k.dasaprakash@gmail.com>
2018-03-20 22:37:32 +0530

    Updated DNN

--- Deep NN/DeepNN_From_Scratch.py
+++ Deep NN/DeepNN_From_Scratch.py
@@ -128,35 +128,40 @@ class DNN_Core:
             gradients['db' + str(i + 1)] = db
         return gradients
 
-    def update_parameters(self, parameters, gradients, alpha):
+    def update_parameters(self, parameters, gradients, alpha, reg_rate, m):
         layers = len(parameters) // 2
         
         for l in range(layers):
+            #parameters["W" + str(l+1)] += (reg_rate/m) * parameters["W" + str(l+1)]
             parameters["W" + str(l+1)] -= alpha * gradients["dW" + str(l+1)]
             parameters["b" + str(l+1)] -= alpha * gradients["db" + str(l+1)]
         return parameters
     
-    def update_full_GD(self, X, Y, epochs, alpha, layer_dims, parameters, print_cost):
+    def feedback(self, X, T, parameters, alpha, reg_rate):
+        A, caches = self.feed_forward(X, parameters)
+        gradients = self.back_propogation(T, A, caches)
+        parameters = self.update_parameters(parameters, gradients, alpha, reg_rate, Y.size)
+        return A, gradients, parameters 
+    
+    def batchGD(self, X, Y, epochs, alpha, layer_dims, parameters, reg_rate, print_cost):
         util = Util()
         J_history = []
         Acc_history = []
         T = util.yEnc(Y)
         for i in range(epochs):
-            A, caches = self.feed_forward(X, parameters)
-            J = self.cost_function(A, T)
-            rate = self.accuracy(A, Y)
-            gradients = self.back_propogation(T, A, caches)
-            parameters = self.update_parameters(parameters, gradients, alpha)
+            A, gradients, parameters = self.feedback(X, T, parameters, alpha, reg_rate)
             if print_cost:
                 if i % 100 == 0:
+                    J = self.cost_function(A, T, parameters, reg_rate)
                     J_history.append(J)
+                    rate = self.accuracy(A, Y)
                     Acc_history.append(rate)
                     print('Iteration: ' + str(i), 'Cost: ' + str(J) + \
                              'Accuracy: ' + str(rate))
                 self.plot_cost(J_history)
         return A, gradients, parameters
     
-    def update_SGD(self, X, Y, epochs, alpha, layer_dims, parameters, batch_size, print_cost):
+    def SGD(self, X, Y, epochs, alpha, layer_dims, parameters, batch_size, reg_rate, print_cost):
         util = Util()
         J_history = []
         Acc_history = []
@@ -165,20 +170,17 @@ class DNN_Core:
             s = np.arange(X.shape[0])
             np.random.shuffle(s)
             X = X[s]
-            Y = Y[s]
             T = T[s]
             for k in range(0, X.shape[0], batch_size):
                 x = X[k:k+batch_size, :]
-                y = Y[k:k+batch_size]
                 t = T[k:k+batch_size, :]
-                A, caches = self.feed_forward(x, parameters)
-                J = self.cost_function(A, t)
-                rate = self.accuracy(A, y)
-                gradients = self.back_propogation(t, A, caches)
-                parameters = self.update_parameters(parameters, gradients, alpha)
+                y = Y[k:k+batch_size]
+                A, gradients, parameters = self.feedback(x, t, parameters, alpha, reg_rate)
             if print_cost:
                 if i % 100 == 0:
+                    J = self.cost_function(A, t, parameters, reg_rate)
                     J_history.append(J)
+                    rate = self.accuracy(A, y)
                     Acc_history.append(rate)
                     print('Iteration: ' + str(i), 'Cost: ' + str(J) + 'Accuracy: ' + str(rate))
                 self.plot_cost(J_history)
@@ -191,25 +193,30 @@ class DNN_Core:
         Yhat = np.argmax(Yhat, axis = 1)
         return np.mean(Y == Yhat)
     
-    def cost_function(self, Yhat, Y):
-        J = -np.sum(Y*(np.log(Yhat)))
+    def cost_function(self, Yhat, Y, parameters, reg_rate):
+        L = len(parameters)//2
+        sum_weights = 0
+        for l in range(L):
+            sum_weights += np.sum(parameters["W" + str(l + 1)])
+        J = (reg_rate / (2*Y.size)) * sum_weights
+        J += -np.sum(Y*(np.log(Yhat)))
         return J
 
 class DNN:
     def __init__(self, layer_dims):
         self.layer_dims = layer_dims
     
-    def fit(self, X, Y, epochs, alpha, layer_dims, batch_type):
+    def fit(self, X, Y, epochs, alpha, layer_dims, batch_type, reg_rate):
         core = DNN_Core(self.layer_dims)
         parameters = core.initialise_parameters()
         if batch_type == 'full':
-            A, gradients, parameters = core.update_full_GD(X, Y, epochs, alpha, layer_dims, parameters, True)
+            A, gradients, parameters = core.batchGD(X, Y, epochs, alpha, layer_dims, parameters, reg_rate, True)
         else:
             if batch_type == 'mini_batch':
                 batch_size = 512
             else:
                 batch_size = 1
-            A, gradients, parameters = core.update_SGD(X, Y, epochs, alpha, layer_dims, parameters, batch_size, True)
+            A, gradients, parameters = core.SGD(X, Y, epochs, alpha, layer_dims, parameters, batch_size, reg_rate, True)
         return A, gradients, parameters
     
     def predict(self, X, parameters):
@@ -238,10 +245,10 @@ deepNN = DNN(layer_dims)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.05, random_state=28) 
 
-Yhat, gradients, parameters = deepNN.fit(X_train, Y_train, 1500, 0.01, layer_dims, 'mini_batch')
+Yhat, gradients, parameters = deepNN.fit(X_train, Y_train, 1500, 0.1, layer_dims, 'mini_batch', 0.1)
 
 accuracy = deepNN.accuracy(Yhat, Y_train)
 
-Yhat_test = deepNN.predict(X_test)
+Yhat_test = deepNN.predict(X_test, parameters)
 
 accuracy_test = deepNN.accuracy(Yhat_test, Y_test)
