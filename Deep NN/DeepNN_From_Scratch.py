5047556aaacf902e839757d5d23cb9f652d23bf1
Das <k.dasaprakash@gmail.com>
2018-03-21 23:06:04 +0530

    Updated DNN

--- Deep NN/DeepNN_From_Scratch.py
+++ Deep NN/DeepNN_From_Scratch.py
@@ -12,23 +12,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 class Util:
+    #Sigmoid function s = 1 / (1+ exp(-z))
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z)), z
     
     def sigmoid_prime(self, z):
         return -np.exp(-z) / (1 + np.exp(-z))**2
-    
-    def relu(self, z):
-        return np.where(z>0, z, 0), z
-        
-    def relu_prime(self, z):
-        return np.where(z>0, 1, 0)
-    
-    def tanh(self, z):
-        return np.tanh(z), z
-    
-    def tanh_prime(self, z):
-        return 1 - np.tanh(z)**2
     
     def softmax(self, z):
         expA = np.exp(z)
@@ -37,6 +26,18 @@ class Util:
     def softmax_prime(self, z):
         s = self.softmax(z)
         return s*(1-s)
+    
+    def tanh(self, z):
+        return np.tanh(z), z
+    
+    def tanh_prime(self, z):
+        return 1 - np.tanh(z)**2
+    
+    def relu(self, z):
+        return np.where(z>0, z, 0), z
+        
+    def relu_prime(self, z):
+        return np.where(z>0, 1, 0)
     
     def yEnc(self, Y):
         col = len(set(Y))
@@ -50,39 +51,54 @@ class DNN_Core:
         
     def initialise_parameters(self):
         parameters = {}
-        for i in range(1, len(self.layer_dims)):
-            W = np.random.randn(self.layer_dims[i-1], self.layer_dims[i])*0.01
-            b = np.random.randn(self.layer_dims[i], 1)*0.01
-            parameters['W' + str(i)] = W
-            parameters['b' + str(i)] = b
+        for l in range(1, len(self.layer_dims)):
+            W = np.random.randn(self.layer_dims[l-1], self.layer_dims[l])*0.01
+            b = np.random.randn(self.layer_dims[l], 1)*0.01
+            parameters['W' + str(l)] = W
+            parameters['b' + str(l)] = b
         return parameters
 
     def linear_forward(self, A, W, b):
         z = np.dot(A, W) + b.T
         return z, (A, W, b)
+    
+    def apply_dropout(self, A, dropout):
+        D = np.random.randn(A.shape[0], A.shape[1])
+        D = D < dropout
+        A = A * D
+        A = A / dropout
+        return A, D
 
-    def activation_forward(self, A, W, b, activation):
+    def activation_forward(self, A, W, b, activation, dropout):
         util = Util()
         Z, linear_cache = self.linear_forward(A, W, b)
         if activation == 'relu':
-            s, activation_cache = util.relu(Z)
+            A, activation_cache = util.relu(Z)
         elif activation == 'tanh':
-            s, activation_cache = util.tanh(Z)
+            A, activation_cache = util.tanh(Z)
         elif activation == 'softmax':
-            s, activation_cache = util.softmax(Z)
+            A, activation_cache = util.softmax(Z)
         else:
-            s, activation_cache = util.sigmoid(Z)
-        return s, (linear_cache, activation_cache)
+            A, activation_cache = util.sigmoid(Z)
+        if dropout == None:
+            dropout_cache = None
+        else:
+            A, dropout_cache = self.apply_dropout(A, dropout)
+        return A, (linear_cache, activation_cache, dropout_cache)
 
-    def feed_forward(self, X, parameters):
+    def feed_forward(self, X, parameters, dropout):
         caches = []
-        l = len(parameters) // 2
+        L = len(parameters) // 2
         A = X
-        for i in range(1, l):
+        for l in range(1, L):
             A_prev = A
-            A, cache = self.activation_forward(A_prev, parameters["W" + str(i)], parameters["b" + str(i)], activation='relu')
+            W = parameters["W" + str(l)]
+            b = parameters["b" + str(l)]
+            A, cache = self.activation_forward(A_prev, W, b, 'relu', dropout)
             caches.append(cache)
-        A, cache = self.activation_forward(A, parameters["W" + str(l)], parameters["b" + str(l)], activation='softmax')
+        W = parameters["W" + str(L)]
+        b = parameters["b" + str(L)]
+        A, cache = self.activation_forward(A, W, b, 'softmax', None)
         caches.append(cache)
         return A, caches
     
@@ -94,9 +110,9 @@ class DNN_Core:
         dA_prev = np.dot(dZ, W.T)
         return dA_prev, dW, db.T
 
-    def activation_backward(self, dA, cache, activation):
+    def activation_backward(self, dA, cache, dropout_cache, activation, dropout):
         util = Util()
-        linear_cache, activation_cache = cache
+        linear_cache, activation_cache, d = cache
         A_prev, W, b = linear_cache
         if activation == 'relu':
             dZ = dA * util.relu_prime(activation_cache)
@@ -107,49 +123,65 @@ class DNN_Core:
         else:
             dZ = dA * util.sigmoid_prime(activation_cache)
         dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+        if dropout_cache is not None:
+            dA_prev = dA_prev * dropout_cache
+            dA_prev = dA_prev / dropout
         return dA_prev, dW, db
+    
+    def get_dropout_cache(self, caches, position):
+        if position > -1:
+            linear_cache, activation_cache, dropout_cache = caches[position]
+        else:
+            dropout_cache = None
+        return dropout_cache
 
-    def back_propogation(self, Y, Yhat, caches):
+    def back_propogation(self, Y, Yhat, caches, dropout):
         L = len(caches)
         gradients = {}
-        
         dA = Yhat-Y
         current_cache = caches[L-1]
-        dA_prev, dW, db = self.activation_backward(dA, current_cache, 'softmax')
+        dropout_cache = self.get_dropout_cache(caches, L-2)
+        dA_prev, dW, db = self.activation_backward(dA, current_cache, dropout_cache, 'softmax', dropout)
         gradients['dA' + str(L-1)] = dA_prev
         gradients['dW' + str(L)] = dW
         gradients['db' + str(L)] = db
         
-        for i in reversed(range(L-1)):
-            current_cache = caches[i]
-            dA_prev, dW, db = self.activation_backward(dA_prev, current_cache, 'softmax')
-            gradients['dA' + str(i)] = dA_prev
-            gradients['dW' + str(i + 1)] = dW
-            gradients['db' + str(i + 1)] = db
+        for l in reversed(range(L-1)):
+            current_cache = caches[l]
+            dropout_cache = self.get_dropout_cache(caches, l-1)
+            dA_prev, dW, db = self.activation_backward(dA_prev, current_cache, dropout_cache, 'relu', dropout)
+            gradients['dA' + str(l)] = dA_prev
+            gradients['dW' + str(l + 1)] = dW
+            gradients['db' + str(l + 1)] = db
         return gradients
 
     def update_parameters(self, parameters, gradients, alpha, reg_rate, m):
         layers = len(parameters) // 2
         
         for l in range(layers):
-            #parameters["W" + str(l+1)] += (reg_rate/m) * parameters["W" + str(l+1)]
+            parameters["W" + str(l+1)] += (alpha * reg_rate/m) * parameters["W" + str(l+1)]
             parameters["W" + str(l+1)] -= alpha * gradients["dW" + str(l+1)]
             parameters["b" + str(l+1)] -= alpha * gradients["db" + str(l+1)]
         return parameters
     
-    def feedback(self, X, T, parameters, alpha, reg_rate):
-        A, caches = self.feed_forward(X, parameters)
-        gradients = self.back_propogation(T, A, caches)
-        parameters = self.update_parameters(parameters, gradients, alpha, reg_rate, Y.size)
-        return A, gradients, parameters 
+    def cost_function(self, Yhat, Y, parameters, reg_rate):
+        L = len(parameters)//2
+        sum_weights = 0
+        for l in range(L):
+            sum_weights += np.sum(parameters["W" + str(l + 1)])
+        J = (reg_rate / (2*Y.size)) * sum_weights
+        J += -np.sum(Y*(np.log(Yhat)))
+        return J
     
-    def batchGD(self, X, Y, epochs, alpha, layer_dims, parameters, reg_rate, print_cost):
+    def batchGD(self, X, Y, epochs, alpha, parameters, reg_rate, dropout, print_cost):
         util = Util()
         J_history = []
         Acc_history = []
         T = util.yEnc(Y)
         for i in range(epochs):
-            A, gradients, parameters = self.feedback(X, T, parameters, alpha, reg_rate)
+            A, caches = self.feed_forward(X, parameters, dropout)
+            gradients = self.back_propogation(T, A, caches, dropout)
+            parameters = self.update_parameters(parameters, gradients, alpha, reg_rate, Y.size)
             if print_cost:
                 if i % 100 == 0:
                     J = self.cost_function(A, T, parameters, reg_rate)
@@ -161,10 +193,9 @@ class DNN_Core:
                 self.plot_cost(J_history)
         return A, gradients, parameters
     
-    def SGD(self, X, Y, epochs, alpha, layer_dims, parameters, batch_size, reg_rate, print_cost):
+    def SGD(self, X, Y, epochs, alpha, parameters, batch_size, reg_rate, dropout, print_cost):
         util = Util()
         J_history = []
-        Acc_history = []
         T = util.yEnc(Y)
         for i in range(epochs):
             s = np.arange(X.shape[0])
@@ -174,16 +205,18 @@ class DNN_Core:
             for k in range(0, X.shape[0], batch_size):
                 x = X[k:k+batch_size, :]
                 t = T[k:k+batch_size, :]
-                y = Y[k:k+batch_size]
-                A, gradients, parameters = self.feedback(x, t, parameters, alpha, reg_rate)
+                A, caches = self.feed_forward(x, parameters, dropout)
+                gradients = self.back_propogation(t, A, caches, dropout)
+                parameters = self.update_parameters(parameters, gradients, alpha, reg_rate, Y.size)
             if print_cost:
                 if i % 100 == 0:
-                    J = self.cost_function(A, t, parameters, reg_rate)
+                    A, caches = self.feed_forward(X, parameters, dropout)
+                    J = self.cost_function(A, T, parameters, reg_rate)
                     J_history.append(J)
-                    rate = self.accuracy(A, y)
-                    Acc_history.append(rate)
-                    print('Iteration: ' + str(i), 'Cost: ' + str(J) + 'Accuracy: ' + str(rate))
+                    rate = self.accuracy(A, Y)
+                    print('Iteration: ' + str(i), 'Cost: ' + str(J), 'Accuracy: ' + str(rate))
                 self.plot_cost(J_history)
+        A, caches = self.feed_forward(X, parameters, dropout)
         return A, gradients, parameters
     
     def plot_cost(self, J_history):
@@ -192,36 +225,29 @@ class DNN_Core:
     def accuracy(self, Yhat, Y):
         Yhat = np.argmax(Yhat, axis = 1)
         return np.mean(Y == Yhat)
-    
-    def cost_function(self, Yhat, Y, parameters, reg_rate):
-        L = len(parameters)//2
-        sum_weights = 0
-        for l in range(L):
-            sum_weights += np.sum(parameters["W" + str(l + 1)])
-        J = (reg_rate / (2*Y.size)) * sum_weights
-        J += -np.sum(Y*(np.log(Yhat)))
-        return J
 
 class DNN:
     def __init__(self, layer_dims):
         self.layer_dims = layer_dims
     
-    def fit(self, X, Y, epochs, alpha, layer_dims, batch_type, reg_rate):
+    def fit(self, X, Y, epochs, alpha, reg_rate, dropout_prob, batch_type):
         core = DNN_Core(self.layer_dims)
         parameters = core.initialise_parameters()
         if batch_type == 'full':
-            A, gradients, parameters = core.batchGD(X, Y, epochs, alpha, layer_dims, parameters, reg_rate, True)
+            A, gradients, parameters = core.batchGD(X, Y, epochs, alpha, \
+                                    parameters, reg_rate, dropout_prob, True)
         else:
             if batch_type == 'mini_batch':
                 batch_size = 512
             else:
                 batch_size = 1
-            A, gradients, parameters = core.SGD(X, Y, epochs, alpha, layer_dims, parameters, batch_size, reg_rate, True)
+            A, gradients, parameters = core.SGD(X, Y, epochs, alpha, parameters,\
+                                    batch_size, reg_rate, dropout_prob, True)
         return A, gradients, parameters
     
-    def predict(self, X, parameters):
+    def predict(self, X, parameters, dropout_prob):
         core = DNN_Core(self.layer_dims)
-        Yhat, caches = core.feed_forward(X, parameters)
+        Yhat, caches = core.feed_forward(X, parameters, dropout_prob)
         return Yhat
     
     def accuracy(self, Yhat, Y):
@@ -231,6 +257,7 @@ class DNN:
     
 
 df = pd.read_csv('../../MNIST_Data/train.csv', sep = ',')
+df = df.iloc[:10000, :]
 X = df.iloc[:, 1:]
 Y = df.iloc[:, 0]
 
@@ -245,10 +272,10 @@ deepNN = DNN(layer_dims)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.05, random_state=28) 
 
-Yhat, gradients, parameters = deepNN.fit(X_train, Y_train, 1500, 0.1, layer_dims, 'mini_batch', 0.1)
+Yhat, gradients, parameters = deepNN.fit(X_train, Y_train, 1500, 0.2, 0, 1, 'mini_batch')
 
 accuracy = deepNN.accuracy(Yhat, Y_train)
 
-Yhat_test = deepNN.predict(X_test, parameters)
+Yhat_test = deepNN.predict(X_test, parameters, 0.8)
 
 accuracy_test = deepNN.accuracy(Yhat_test, Y_test)
